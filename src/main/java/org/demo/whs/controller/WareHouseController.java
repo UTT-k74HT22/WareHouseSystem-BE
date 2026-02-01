e9edb427df04541dad1a10027a8c17eb218104cb
DungDev <hoangdinhdung0205@gmail.com>
2026-02-01 11:12:34 +0700

    feat: implement api getList wh

--- src/main/java/org/demo/whs/controller/WareHouseController.java
+++ src/main/java/org/demo/whs/controller/WareHouseController.java
@@ -12,6 +12,7 @@ import org.demo.whs.entity.dto.response.WareHouse.WareHouseResponse;
 import org.demo.whs.service.WareHouseService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RequestMapping("/api/v1/warehouse")
 @RestController
@@ -54,6 +55,19 @@ public class WareHouseController {
         log.info("Fetching all warehouses - page: {}, size: {}", page, size);
         PageResponse<WareHouseResponse> response = wareHouseService.getAll(page, size);
         BaseResponse<PageResponse<WareHouseResponse>> baseResponse = BaseResponse.success(response);
+        return ResponseEntity.ok(baseResponse);
+    }
+
+    /**
+     * Endpoint to retrieve all warehouses without pagination.
+     *
+     * @return list of warehouse responses
+     */
+    @GetMapping("/all")
+    public ResponseEntity<BaseResponse<List<WareHouseResponse>>> getAll() {
+        log.info("Fetching all warehouses without pagination");
+        List<WareHouseResponse> response = wareHouseService.getWareHouses();
+        BaseResponse<List<WareHouseResponse>> baseResponse = BaseResponse.success(response);
         return ResponseEntity.ok(baseResponse);
     }
 
