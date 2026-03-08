d21a68db30424df39a21e7860941f8fa39e25743
NguyenQuocToan05 <nguyenquoctoan.bv@gmail.com>
2026-03-08 23:46:18 +0700

    fix

--- src/main/java/org/demo/whs/entity/dto/request/BusinessPartner/SearchBusinessPartnerRequest.java
+++ src/main/java/org/demo/whs/entity/dto/request/BusinessPartner/SearchBusinessPartnerRequest.java
@@ -2,9 +2,13 @@ package org.demo.whs.entity.dto.request.BusinessPartner;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.Setter;
 import org.demo.whs.entity.enums.BusinessPartnerStatus;
 import org.demo.whs.entity.enums.BusinessPartnerType;
 
+@Getter
+@Setter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class SearchBusinessPartnerRequest {
 
