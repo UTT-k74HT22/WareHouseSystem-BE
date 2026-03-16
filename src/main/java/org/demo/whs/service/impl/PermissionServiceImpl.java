4a7476c2ca037d972794f432945cc069c453463b
NguyenQuocToan05 <nguyenquoctoan.bv@gmail.com>
2026-03-17 01:07:54 +0700

    Done API ấy chi tiết permission

--- src/main/java/org/demo/whs/service/impl/PermissionServiceImpl.java
+++ src/main/java/org/demo/whs/service/impl/PermissionServiceImpl.java
@@ -2,10 +2,14 @@ package org.demo.whs.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.demo.whs.entity.Permission;
 import org.demo.whs.entity.dto.request.Permission.CreatePermissionRequest;
 import org.demo.whs.entity.dto.request.Permission.UpdatePermissionRequest;
 import org.demo.whs.entity.dto.response.PageResponse;
 import org.demo.whs.entity.dto.response.Permission.PermissionResponse;
+import org.demo.whs.exception.ErrorCode;
+import org.demo.whs.exception.NotFoundException;
+import org.demo.whs.mapper.PermissionMapper;
 import org.demo.whs.repository.PermissionRepository;
 import org.demo.whs.service.PermissionService;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +21,7 @@ import org.springframework.stereotype.Service;
 public class PermissionServiceImpl implements PermissionService {
 
     private final PermissionRepository permissionRepository;
+    private final PermissionMapper permissionMapper;
 
     @Override
     public PermissionResponse createPermission(CreatePermissionRequest request) {
@@ -28,9 +33,19 @@ public class PermissionServiceImpl implements PermissionService {
         return null;
     }
 
+    /**
+     * Get a permission by its ID.
+     *
+     * @param id the ID of the permission
+     * @return the permission response
+     */
     @Override
     public PermissionResponse getPermissionById(String id) {
-        return null;
+
+        Permission permission = permissionRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.PERM_001));
+
+        return permissionMapper.toResponse(permission);
     }
 
     @Override
